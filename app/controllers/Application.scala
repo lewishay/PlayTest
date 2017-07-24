5fcdce64052e7120607b626bfb60c5f35f4887a0
lewishayqa <lewis.hay@qa.com>
2017-07-24 17:13:22 +0000

    Monday 24/07 basic test with Play

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,41 @@ import play.api._
 import play.api.mvc._
 
 class Application extends Controller {
-
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
 
+  def out = Action {
+    Redirect("http://hmpg.net/", 302)
+  }
+
+  def number(numb: Int) = Action {
+    Ok(views.html.numberPage(numb))
+  }
+
+  def notFound = Action {
+    NotFound("Page not found")
+  }
+
+  def badRequest = Action {
+    BadRequest("Oops! An error occured.")
+  }
+
+  def internalServ = Action {
+    InternalServerError("Error with the internal server...what have you done?")
+  }
+
+  def customStatus = Action {
+    Status(666)("You are attempting to unleash hell. Please don't do this.")
+  }
+
+  def result1 = Action {
+    Ok("This is the first result.")
+  }
+
+  def result2 = Action {
+    Ok("This is the second result.")
+  }
+
+  val coolPage = TODO
 }
--- build.sbt
+++ build.sbt
@@ -10,6 +10,7 @@ scalaVersion := "2.11.11"
 
 libraryDependencies ++= Seq( jdbc , cache , ws , specs2 % Test )
 
-unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
+unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )
 
-      
+
+
