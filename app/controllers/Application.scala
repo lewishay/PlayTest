3355f34c76a98c6ca914fc71aaaf754a8f85ab89
lewishayqa <lewis.hay@qa.com>
2017-07-25 17:16:35 +0000

    Significant additions to all aspects of the project

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,27 +5,15 @@ import play.api.mvc._
 
 class Application extends Controller {
   def index = Action {
-    Ok(views.html.index())
+    Ok(views.html.index()).withSession("mySession" -> "Lewis")
   }
 
   def out = Action {
     Redirect("http://hmpg.net/", 302)
   }
 
-  def number(numb: Int) = Action {
-    Ok(views.html.numberPage(numb))
-  }
-
-  def notFound = Action {
-    NotFound("Page not found")
-  }
-
   def badRequest = Action {
     BadRequest("Oops! An error occured.")
-  }
-
-  def internalServ = Action {
-    InternalServerError("Error with the internal server...what have you done?")
   }
 
   def customStatus = Action {
@@ -40,5 +28,41 @@ class Application extends Controller {
     Ok("This is the second result.")
   }
 
-  val coolPage = TODO
+  def hello(name: String) = Action {
+    Ok("Hello " + name + "!")
+  }
+
+  def helloSuperUser() = Action {
+    Redirect(routes.Application.hello("SuperUser"))
+  }
+
+  def number(numb: Int) = Action {
+    Ok("Your number is " + numb + "!").discardingCookies(DiscardingCookie("SuperCookie"))
+  }
+
+  def listMonths(numMonths: Option[Int]) = Action {
+    val months = List("January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+    "November", "December")
+    numMonths match {
+      case Some(x) => if(x > 12) {
+        Ok("Error, invalid number")
+      }
+      else {
+        Ok("The months of the year are " + months.slice(0, x))
+      }
+      case None => Ok("The months of the year are " + months)
+    }
+  }
+
+  def getSessionName = Action {
+    request => request.session.get("mySession").map {
+      user => Ok("The user of this session is: " + user)
+    }.getOrElse {
+      Unauthorized("Oops, you are not connected")
+    }
+  }
+
+  def removeSession = Action {
+      Ok("Session removed.").withNewSession
+  }
 }
---
+++ public/javascripts/index.js
@@ -0,0 +1,23 @@
+function yourName() {
+    if(document.getElementById('theName').value == "") {
+        $("#nameForm").attr("action", "/hello");
+    }
+    else {
+        var path = "/hello/" + document.getElementById('theName').value;
+        $("#nameForm").attr("action", path);
+    }
+}
+
+function yourNumber() {
+    var path = "/numbers/" + document.getElementById('theNumber').value;
+    $("#numberForm").attr("action", path);
+}
+
+function numMonths() {
+    if(document.getElementById('theMonths').value == "") {
+        $("#monthsForm").attr("action", "/months");
+    }
+    else {
+        window.open("http://localhost:9000/months?numMonths=" + document.getElementById("theMonths").value);
+    }
+}
